d142b7bacf9e035a25898901d1c83011a2373d35
Frederik Lux <f.lux@phytec.de>
2022-06-29 09:40:15 +0200

    module remove unused implementation of bootstrap

--- Module.php
+++ Module.php
@@ -4,11 +4,11 @@ namespace flux711\yii2\facility_phptool_code_dev;
 
 use flux711\yii2\facility_phptool_code_dev\models\FacilityCodePool;
 
-class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
+class Module extends \yii\base\Module
 {
 
 	public $controllerNamespace = 'flux711\yii2\facility_phptool_code_dev\controllers';
-	
+
 	public function checkCode($code, $pool)
 	{
 		return FacilityCodePool::checkCode($code, $pool);
